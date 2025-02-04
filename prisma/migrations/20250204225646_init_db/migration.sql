0bcb316beffdcd909950a166d2ffa92be76f12a5
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2025-02-04 20:01:01 -0300

    Creation of prisma schema and first migration

---
+++ prisma/migrations/20250204225646_init_db/migration.sql
@@ -0,0 +1,157 @@
+-- CreateEnum
+CREATE TYPE "MachineOperatorLevel" AS ENUM ('WORKER', 'LEADER');
+
+-- CreateEnum
+CREATE TYPE "MachineStatus" AS ENUM ('FORA_DE_PRODUCAO', 'PRODUZINDO', 'EM_SETUP', 'EM_MANUTENCAO', 'FORA_DE_TURNO');
+
+-- CreateTable
+CREATE TABLE "Sector" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Sector_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MachineOperator" (
+    "id" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "level" "MachineOperatorLevel" NOT NULL,
+    "sectorId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "MachineOperator_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Machine" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "sectorId" TEXT NOT NULL,
+    "status" "MachineStatus" NOT NULL,
+    "workOrderOperationId" TEXT,
+    "machineOperatorId" TEXT,
+    "lastReportId" TEXT,
+    "lastReportTime" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Machine_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "SellOrder" (
+    "id" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "clientName" TEXT NOT NULL,
+    "sellerName" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "emissionDate" TIMESTAMP(3) NOT NULL,
+    "deliveryDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "SellOrder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WorkOrder" (
+    "id" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "sellOrderId" TEXT,
+    "status" TEXT NOT NULL,
+    "deliveryDate" TIMESTAMP(3) NOT NULL,
+    "productName" TEXT NOT NULL,
+    "productDescription" TEXT NOT NULL,
+    "comments" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "WorkOrder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WorkOrderOperation" (
+    "id" TEXT NOT NULL,
+    "workOrderId" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "balance" INTEGER NOT NULL,
+    "productionBegin" TIMESTAMP(3),
+    "productionEnd" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "WorkOrderOperation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Report" (
+    "id" TEXT NOT NULL,
+    "machineId" TEXT NOT NULL,
+    "machineOperatorId" TEXT NOT NULL,
+    "workOrderOperationId" TEXT,
+    "setupOperatorId" TEXT,
+    "reportTime" TIMESTAMP(3) NOT NULL,
+    "elapsedTimeInSeconds" INTEGER NOT NULL,
+    "type" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Report_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Sector_id_key" ON "Sector"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MachineOperator_id_key" ON "MachineOperator"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Machine_id_key" ON "Machine"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "SellOrder_id_key" ON "SellOrder"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WorkOrder_id_key" ON "WorkOrder"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WorkOrderOperation_id_key" ON "WorkOrderOperation"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Report_id_key" ON "Report"("id");
+
+-- AddForeignKey
+ALTER TABLE "MachineOperator" ADD CONSTRAINT "MachineOperator_sectorId_fkey" FOREIGN KEY ("sectorId") REFERENCES "Sector"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Machine" ADD CONSTRAINT "Machine_sectorId_fkey" FOREIGN KEY ("sectorId") REFERENCES "Sector"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Machine" ADD CONSTRAINT "Machine_machineOperatorId_fkey" FOREIGN KEY ("machineOperatorId") REFERENCES "MachineOperator"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Machine" ADD CONSTRAINT "Machine_workOrderOperationId_fkey" FOREIGN KEY ("workOrderOperationId") REFERENCES "WorkOrderOperation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkOrder" ADD CONSTRAINT "WorkOrder_sellOrderId_fkey" FOREIGN KEY ("sellOrderId") REFERENCES "SellOrder"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkOrderOperation" ADD CONSTRAINT "WorkOrderOperation_workOrderId_fkey" FOREIGN KEY ("workOrderId") REFERENCES "WorkOrder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Report" ADD CONSTRAINT "Report_machineId_fkey" FOREIGN KEY ("machineId") REFERENCES "Machine"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Report" ADD CONSTRAINT "Report_workOrderOperationId_fkey" FOREIGN KEY ("workOrderOperationId") REFERENCES "WorkOrderOperation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Report" ADD CONSTRAINT "Report_machineOperatorId_fkey" FOREIGN KEY ("machineOperatorId") REFERENCES "MachineOperator"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
