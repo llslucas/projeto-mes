26432ccac79693c5e649fb002c9fd18a52d9d8c2
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2025-02-09 16:10:13 -0300

    Added repositories do database.module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,9 +1,76 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma/prisma.service";
+import { MachineOperatorRepository } from "@/domain/mes/application/repositories/machine-operator-repository";
+import { PrismaMachineOperatorRepository } from "./prisma/repositories/prisma-machine-operator-repository";
+import { MachineRepository } from "@/domain/mes/application/repositories/machine-repository";
+import { ProductionReportRepository } from "@/domain/mes/application/repositories/production-report-repository";
+import { SectorRepository } from "@/domain/mes/application/repositories/sector-repository";
+import { SellOrderRepository } from "@/domain/mes/application/repositories/sell-order-repository";
+import { SetupReportRepository } from "@/domain/mes/application/repositories/setup-report-repository";
+import { ShiftReportRepository } from "@/domain/mes/application/repositories/shift-report-repository";
+import { WorkOrderOperationRepository } from "@/domain/mes/application/repositories/work-order-operation-repository";
+import { WorkOrderRepository } from "@/domain/mes/application/repositories/work-order-repository";
+import { PrismaMachineRepository } from "./prisma/repositories/prisma-machine-repository";
+import { PrismaProductionReportRepository } from "./prisma/repositories/prisma-production-report-repository";
+import { PrismaSectorRepository } from "./prisma/repositories/prisma-sector-repository";
+import { PrismaSellOrderRepository } from "./prisma/repositories/prisma-sell-order-repository";
+import { PrismaSetupReportRepository } from "./prisma/repositories/prisma-setup-report-repository";
+import { PrismaShiftReportRepository } from "./prisma/repositories/prisma-shift-report-repository";
+import { PrismaWorkOrderOperationRepository } from "./prisma/repositories/prisma-work-order-operation-repository";
+import { PrismaWorkOrderRepository } from "./prisma/repositories/prisma-work-order-repository";
 
 @Module({
   imports: [],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: SectorRepository,
+      useClass: PrismaSectorRepository,
+    },
+    {
+      provide: MachineRepository,
+      useClass: PrismaMachineRepository,
+    },
+    {
+      provide: MachineOperatorRepository,
+      useClass: PrismaMachineOperatorRepository,
+    },
+    {
+      provide: SellOrderRepository,
+      useClass: PrismaSellOrderRepository,
+    },
+    {
+      provide: WorkOrderRepository,
+      useClass: PrismaWorkOrderRepository,
+    },
+    {
+      provide: WorkOrderOperationRepository,
+      useClass: PrismaWorkOrderOperationRepository,
+    },
+    {
+      provide: ProductionReportRepository,
+      useClass: PrismaProductionReportRepository,
+    },
+    {
+      provide: SetupReportRepository,
+      useClass: PrismaSetupReportRepository,
+    },
+    {
+      provide: ShiftReportRepository,
+      useClass: PrismaShiftReportRepository,
+    },
+  ],
+  exports: [
+    PrismaService,
+    SectorRepository,
+    MachineRepository,
+    MachineOperatorRepository,
+    SellOrderRepository,
+    WorkOrderRepository,
+    WorkOrderOperationRepository,
+    ProductionReportRepository,
+    SetupReportRepository,
+    ShiftReportRepository,
+  ],
 })
 export class DatabaseModule {}
